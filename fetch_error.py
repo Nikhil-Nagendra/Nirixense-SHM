a0c231db6e577f9972a846878a91f90f3f65e0e3
Nikhil Nagendra <nikearmy22@gmail.com>
2026-03-17 13:54:19 +0530

    Project cleanup: removed temporary test scripts and logs before submission

--- fetch_error.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import urllib.error
-try:
-    response = urllib.request.urlopen('http://localhost:8000/api/nodes/')
-    print(response.read().decode())
-except urllib.error.HTTPError as e:
-    print(e.read().decode())
